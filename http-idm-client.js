fd95d252280bef14bb8c62d2a24b0435820a7bb0
david <nopbyte>
2016-09-02 10:03:18 +0200

    make it promisse-based

--- http-idm-client.js
+++ http-idm-client.js
@@ -12,55 +12,32 @@ var HTTPIDMClient = function (configuration) {
 };
 
 
-HTTPIDMClient.prototype.registerEntity = function(entity, callbackRegisterEntityFinished){
-
-
-
-}
-
-HTTPIDMClient.prototype.authenticateEntity = function(authentication_type, token, onAuthenticationFinished){
-
+HTTPIDMClient.prototype.authenticateEntityPromisse = function(token){
     var url =  this.url;
-    /*if(authentication_type && authentication_type !=null ){
-      url = url  +authentication_type;
-    }*/
-    //console.log(url + token);
-  	var options = {
-                          url: url,
-                          headers: {'Authorization': "bearer "+ token,
-                  				   "User-Agent": "user-agent",
-                  				  'content-type': 'application/json'
-                          }
-          };
-  	function handleHttpResponse(onAuthenticationFinished, error, response, body) {
-      			//console.log("args from IDM url:  "+JSON.stringify(arguments));
-            if (!error && response.statusCode == 200) {
+    var options = {     url: url,
+                        headers: {'Authorization': "bearer "+ token,
+                           "User-Agent": "user-agent",
+                          'content-type': 'application/json'
+                  }
+    };
 
-  						var result = {"success":true,"error":"","data":{}};
-  						res = JSON.parse(body);
-  						result.data = res;
-  						onAuthenticationFinished(result);
-  						return;
-  				  }
-  				  else if (!error) {
-  			 	  	 var result = {"success":false,
-  			 								     "error": "wrong satus code from authentication: "+response.statusCode+" error "+ body
-  					   };
-  					   if(response.statusCode == 401){
-  					  	result.error = "bad credentials for authentication";
-  					   }
-  					   onAuthenticationFinished(result);
-  					   return;
-  				 }
-           else{
-  					  var result = {"success":false,"error":JSON.stringify(error)};
-  					  onAuthenticationFinished(result);
-  					  return;
+    var promisse = new Promise((resolve, reject) => {
+        request.get(options,function(onAuthenticationFinished, error, response, body){
+          if(!error && response.statusCode == 200){
+            return resolve (result);
           }
-  			}
-  	request.get(options,handleHttpResponse.bind(this,onAuthenticationFinished));
-
-
+          else if(!error){
+            if(response.statusCode == 401)
+                return reject(new Error("bad credentials for authentication"));
+            else
+                return reject(new Error("wrong satus code from authentication: "+response.statusCode+" error "+ body));
+         }
+         else{
+            return reject(new Error(JSON.stringify(error)));
+        }
+        }.bind(this,onAuthenticationFinished));
+    });
+    return promisse;
 }
 
 module.exports = HTTPIDMClient;
